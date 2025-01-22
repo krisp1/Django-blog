62aa72dc19a0e5ccf2360328b0ded9ecaf557fdc
krisp1 <mrkph01@gmail.com>
2025-01-22 15:13:12 +1100

    rm sec

--- blog/views.py
+++ blog/views.py
@@ -48,7 +48,7 @@ def post_detail(request, year, month, day, post):
     
     # List of similar posts
     posts_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    similar_posts = Post.published.filter(tags__in=posts_tags_ids)\
                                   .exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
                                  .order_by('-same_tags','-publish')[:4]
